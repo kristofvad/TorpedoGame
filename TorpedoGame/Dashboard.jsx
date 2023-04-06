db2191d67fa35fa6d37314d88f5b2275bcc23b5f
kristofvad <kristof.vad@gmail.com>
2023-04-06 17:56:48 +0200

    New file

---
+++ TorpedoGame/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Dashboard() {
+    
+    return (
+      <div>
+        <h3>You have not set any sessions</h3>
+      </div>
+    )
+  }
+  
+  export default Dashboard
